05e8d7114a93fd922a0531e51d13c9bac53cd043
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-04-30 18:28:53 -0700

    dotenv.config

--- api-src/index.js
+++ api-src/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: 'MONGO_URI' });
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
