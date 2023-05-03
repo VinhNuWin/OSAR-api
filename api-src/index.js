d575bf167a7e9cb921e81be85aefcb82e07a393a
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-05-02 17:33:27 -0700

    cors test

--- api-src/index.js
+++ api-src/index.js
@@ -22,11 +22,11 @@ app.use('/users', usersRoutes);
 app.use('/incidents', incidentRoutes);
 app.use('/assailants', assailantRoutes);
 
-// app.use(function(req,res,next) {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Method', 'GET,PUT,POST,DELETE');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Application/json');
-// });
+app.use(function(req,res,next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Method', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Application/json');
+});
 
 app.use((req,res, next) => {
     console.log(req.path, req.method)
