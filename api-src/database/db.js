cee4403df968d35ab01ea10acf36298e19c275a1
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-05-02 19:03:00 -0700

    added try catch to mongoose connect

--- api-src/database/db.js
+++ api-src/database/db.js
@@ -1,8 +1,14 @@
 const { MongoClient } = require('mongodb');
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {})
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+    .then(() => {
+        console.log("mongoDB connected")
+    })
     
     .catch((error) => {
         console.log('not connected to mongodb server')
