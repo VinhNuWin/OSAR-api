e4beb7d5a428dd49749556977ca1b278cf3d9d6d
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-06-21 17:31:17 -0700

    retry to revert

--- api-src/database/registry.js
+++ api-src/database/registry.js
@@ -48,9 +48,8 @@ const updateRegistry = async (req,res) => {
         return res.status(404).json({error: 'no such registry'})
     }
 
-    const registry = await Registry.findOneAndUpdate({_id: id}, 
-        {
-        ...registry
+    const registry = await Registry.findOneAndUpdate({_id: id}, {
+        ...req.body
     });
     res.status(200).json(registry);
     
